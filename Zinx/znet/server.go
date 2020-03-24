39fa6c8f064e6b0d561b2b0a31f0b923cbc4bff7
micor <micor5688@163.com>
2020-03-24 11:28:01 +0800

    启动客户端的业务

--- Zinx/znet/server.go
+++ Zinx/znet/server.go
@@ -55,7 +55,7 @@ func (s *Server) Start() {
 						fmt.Println("recive buf err", err)
 						continue
 					}
-
+					fmt.Printf("recv client buf %s,cnt %d\n", buf, index)
 					//回显功能
 					if _, err := conn.Write(buf[:index]); err != nil {
 						fmt.Println("wite back buf err:", err)
@@ -71,7 +71,7 @@ func (s *Server) Start() {
 func (s *Server) Stop() {
 	fmt.Println("[STOP] Zinx server , name ", s.Name)
 
-	//将其他需要清理的连接信息或者其他信息 也要一并停止或者清理
+	//TODO:将其他需要清理的连接信息或者其他信息 也要一并停止或者清理
 
 }
 
---
+++ ZinxV0.1/client.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"time"
+)
+
+/*
+模拟客户端
+*/
+
+func main() {
+	fmt.Println("client starting ....")
+	//1.直接远程连接服务器，得到一个conn连接
+	conn, err := net.Dial("tcp", "192.168.73.1:8080")
+	if err != nil {
+		fmt.Println("client start server faild err=", err)
+		return
+	}
+	for {
+		//2.直接调用write写数据
+		_, err := conn.Write([]byte("Hello Zinx V0.1"))
+		if err != nil {
+			fmt.Println("write conn faild err=", err)
+			return
+		}
+		buf := make([]byte, 1024)
+		cnt, err := conn.Read(buf)
+		if err != nil {
+			fmt.Printf("write conn faild err=", err)
+			return
+		}
+		fmt.Printf("server call back :%s,cnt=%d\n", buf, cnt)
+		//cpu阻塞
+		time.Sleep(1 * time.Second)
+	}
+
+}
